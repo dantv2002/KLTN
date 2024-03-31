c38275ac8bf9f8970d69765e84247cd933defd40
HT-Tuan <huynhthanhtuan2206@gmail.com>
2024-03-31 23:21:58 +0700

    feat: add server springboot v3.2.4 for project EMR

---
+++ server/springboot/emr/src/test/java/com/hospitalx/emr/EmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalx.emr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
