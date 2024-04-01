f2452faf35b90617aa1ecf2e6d3eeb5e1ea8a783
HT-Tuan <huynhthanhtuan2206@gmail.com>
2024-04-01 23:43:52 +0700

    feat: add connect MongoDB database function

---
+++ server/springboot/emr/src/main/java/com/hospitalx/emr/configs/DatabaseConfig.java
@@ -0,0 +1,51 @@
+package com.hospitalx.emr.configs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoIterable;
+
+/**
+ * DatabaseConfig
+ */
+@Configuration
+public class DatabaseConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        MongoClient client = MongoClients.create(uri);
+        try {
+            MongoIterable<String> databases = client.listDatabaseNames();
+            List<String> resultdb = new ArrayList<>();
+            for (String dbname : databases) {
+                resultdb.add(dbname);
+            }
+            logger.info("MongoDB database connected. List databases name: " + resultdb);
+        } catch (Exception e) {
+            logger.error("Failed to connect mongoDB database: " + e);
+        }
+        return client;
+    }
+
+}
