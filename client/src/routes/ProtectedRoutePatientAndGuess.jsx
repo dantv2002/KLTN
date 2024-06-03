bf4f2a972964c483f2714e01186d75083c0e010c
Thanh Dat <caitlyn.ochamp@gmail.com>
2024-06-03 21:07:41 +0700

    update protected route

---
+++ client/src/routes/ProtectedRoutePatientAndGuess.jsx
@@ -0,0 +1,30 @@
+import Cookies from "js-cookie";
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ProtectedRoutePatientAndGuess = ({ children }) => {
+  
+    const role = Cookies.get("Role")
+
+    if (role === undefined || role === "PATIENT") {
+        return children
+    }
+    if (role === "ADMIN"){
+        return <Navigate to='/admin/statistic' replace />
+    }
+    if (role === "DOCTOR"){
+        return <Navigate to='/doctor/medical' replace />
+    }
+    if (role === "NURSE"){
+        return <Navigate to='/nurse/medical' replace />
+    }
+    if (role === "RECEPTIONIST"){
+        return <Navigate to='/receptionist/records' replace />
+    }
+}
+
+export default ProtectedRoutePatientAndGuess
+
+ProtectedRoutePatientAndGuess.propTypes = {
+    children: PropTypes.node.isRequired,
+};
